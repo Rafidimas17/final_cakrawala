3a84f60f8807161bc4a9bc35f4c2e3a9daebaef7
Rafidimas17 <rafidimas1706@gmail.com>
2023-06-21 08:35:30 +0700

    add-app

--- backend/app.js
+++ backend/app.js
@@ -50,7 +50,7 @@ app.use('/sb-admin-2',express.static(path.join(__dirname, 'node_modules/startboo
 app.use(cors())
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
